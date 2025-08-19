be56430650d9e1b1e00936bf81a38eb113abb956
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-08-19 10:16:30 -0300

    ✨ feat(auth): cria rota /auth/me para retornar dados do usuário logado e decodificar JWT

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,58 @@
+import { auth, clerkClient } from "@clerk/nextjs/server"
+import jwt from "jsonwebtoken"
+import { NextResponse } from "next/server"
+
+export async function GET(req: Request) {
+  try {
+    const { userId, sessionId, getToken } = await auth()
+
+    if (!userId || !sessionId) {
+      return NextResponse.json({ success: false, error: "Não autenticado" }, { status: 401 })
+    }
+
+    const client = clerkClient()
+    const user = (await client).users.getUser(userId)
+
+    const token = await getToken({ template: "default" })
+    let decodedToken: object | null = null
+
+    if (token) {
+      decodedToken = jwt.decode(token) as object
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        user: {
+          id: (await user).id,
+          name: (await user).firstName,
+          email: (await user).emailAddresses[0]?.emailAddress,
+          createdAt: (await user).createdAt,
+        },
+        sessionId,
+        token: decodedToken,
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    console.error("❌ Erro ao buscar usuário:", error)
+
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: error.message,
+        },
+        { status: 400 }
+      )
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Erro desconhecido",
+      },
+      { status: 500 }
+    )
+  }
+}
